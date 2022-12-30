ab16ea4d83199fe3b9a523be550f3b03624361f4
wesleygoyette <wesley.goyette@gmail.com>
2022-12-30 16:24:42 -0700

    just messing around

--- sketch.js
+++ sketch.js
@@ -1,12 +1,37 @@
-function setup() {
-  createCanvas(400, 400);
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    noStroke()
+    background(23);
+    lx = 0;
+    ly = 0;
+    lastPressed = 0;
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function draw(){
+
+    if(mouseIsPressed && frameCount-lastPressed > 1 && mouseX != 0 && mouseY != 0){
+
+        if(lx==0&&ly==0){
+
+            lx = mouseX;
+            ly = mouseY;
+        }
+        else{
+
+            stroke(255);
+            strokeWeight(10);
+            line(lx,ly,mouseX,mouseY);
+            noStroke();
+        }
+
+
+        
+
+        lastPressed = frameCount;
+
+        lx = mouseX;
+        ly = mouseY;
+    }
+
+    
 }
